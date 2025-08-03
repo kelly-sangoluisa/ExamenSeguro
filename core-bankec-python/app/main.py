a7c0f1b14074c917af718b2b278ebbf1ceba8b8b
kelly-sangoluisa <kelly.sangoluisa@epn.edu.ec>
2025-08-03 09:05:55 -0500

    Mitigar Hallazgos H01 y  H02

--- core-bankec-python/app/main.py
+++ core-bankec-python/app/main.py
@@ -610,4 +610,4 @@ def initialize_db():
         g.db_initialized = True
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="127.0.0.1", port=8000, debug=False)
